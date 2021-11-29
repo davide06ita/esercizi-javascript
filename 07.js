af05cf3496cc0a2cdad8cd36d31baf184fa1a14a
davide nesto <d.nesto@donboscosandona.it>
2021-11-29 13:03:53 +0100

    c

---
+++ 07.js
@@ -0,0 +1,17 @@
+// Scrivere un programma che dati
+// il prezzo del biglietto
+// e il numero di persone
+// calcoli il totale da pagare
+// e lo scriva in una frase
+
+// scrivere il prezzo del biglietto per ogni persona 
+let prezzo = 5.50;
+
+//scrivere il numero di persone che devono acquistare il biglietto
+let numero = 6;
+
+// calcolo il prezzo
+totale = prezzo * numero;
+
+//scrivi una frase 
+console.log("Il totale da pagare è di " + totale + " €");
